0ba1620dbe84037e5f0e172cdca55c9c39a14c25
Yasmim Sampaio <yasmimsampaio10@hotmail.com>
2020-10-30 15:22:09 -0400

    created buttons, questions, and answers

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-// Start Buttom
+// Start Buttom, Timer
 var startBtn = document.getElementById("Start");
+var question = document.getElementById("Question");
+var answers1 = document.getElementById("Answer1");
+var answers2 = document.getElementById("Answer2");
+var answers3 = document.getElementById("Answer3");
+var answers4 = document.getElementById("Answer4");
+
 var secondsLeft = 10;
 
 function startBtnClick() {
@@ -14,6 +20,23 @@ function startBtnClick() {
         }
     
       }, 1000);
+      
+      var answerBtnOne = document.createElement("button");
+      answerBtnOne.innerText = "Answer1";
+      var answerBtnTwo = document.createElement("button");
+      answerBtnTwo.innerText = "Answer2";
+      var answerBtnThree = document.createElement("button");
+      answerBtnThree.innerText = "Answer3";
+      var answerBtnFour = document.createElement("button");
+      answerBtnFour.innerText = "Answer4";
+
+
+    question.append("Question1");
+    answers1.append(answerBtnOne, "Answer1");
+    answers2.append(answerBtnTwo, "Answer2");
+    answers3.append(answerBtnThree, "Answer3");
+    answers4.append(answerBtnFour, "Answer4");
+
     
     console.log("Button Click");
 }
